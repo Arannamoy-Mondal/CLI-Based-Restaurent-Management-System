fed2e942679b0436113f33e009761033e008d91f
Arannamoy Mondal <71834368+Arannamoy-Mondal@users.noreply.github.com>
2024-10-16 21:02:29 +0600

    Add files via upload

--- user.py
+++ user.py
@@ -33,13 +33,17 @@ class Admin(User):
     def viewEmployee(self,restaurent):
         restaurent.viewEmployee()
 
-
+    def addNewItem(self,res,item):
+        name=item.name
+        price=item.price
+        quantity=item.quantity
+        res.menu.addItem(name,price,quantity)
 # Restaurent class
 class Restaurent:
     def __init__(self,name):
         self.name=name
         self.employeeList=[]
-        
+        self.menu=Menu()
     def addEmployee(self,employee):
          self.employeeList.append(employee)
          print(f'\n\tEmployee add successfully.')   
@@ -54,26 +58,97 @@ class Menu:
     def __init__(self):
         self.itemList=[]
     
-    def addItem(self,item):
+    def addItem(self,name,price,quantity):
+        item=Item(name,price,quantity)
         self.itemList.append(item)
+        return item
     
     def showItem(self):
-        print(self.itemList)
+        for it in self.itemList:
+            print(f'Name: {it.name}, Price: {it.price}$, Quantity: {it.quantity}')
 
     def findItem(self,item):
         for i in self.itemList:
             if item.lower()==i.name.lower():
-                return i
+                return i  
         return None
     
     def removeItem(self,itemName):
         item=self.findItem(itemName)
         if item:
             self.itemList.remove(item)
-            print('Item deleted')
+            print('tItem deleted')
         else:
-            print('Item not found')
-# admin1=Admin('Admin1','12345678912','admin1@admin.ca','Cape Breton, Nova Scotia')
+            print('\tItem not found')
+
+
+# item class
+class Item:
+    def __init__(self,name,price,quantity):
+        self.name=name
+        self.price=price
+        self.quantity=quantity
+
+# customer class
+
+class Customer(User):
+    def __init__(self, name, phone, email, address):
+        super().__init__(name, phone, email, address)
+        self.cart=Order()
+
+    def UserShowItem(self,res):
+        res.menu.showItem()
+
+    def UserAddToCart(self,restaurant,item_name,quantity):
+        item=restaurant.findItem(item_name)
+        if item:
+            item.quantity=quantity
+            self.cart.addItem(item)
+            print('Add successfully.')
+        else:
+            print('Item not found.')
+
+    def UserViewCart(self):
+        print('\tView Cart')
+        print('Name\tPrice\tQuantity')
+        for item,quantity in self.cart.items.items():
+            print(f'{item.name}\t{item.price}\t{quantity}')
+        print(f'Total Price{self.cart.totalPrice}')
+
+
+# Order Class
+class Order:
+    def __init__(self):
+        self.items={}
+    
+    def addItem(self,item):
+        if item in self.item:
+            self.items[item]+=item.quantity # if item is available in cart
+        else:
+            self.items[item]+=item.quantity
+
+    def removeItem(self,item):
+        if item in self.items:
+            # self.items.remove(item)
+            del self.items[item]     
+
+    def totalPrice(self,item):
+        return(item.price*quantity for item,quantity in self.items.items())
+
+    def clear(self):
+        self.items={}
+
+admin1=Admin('Admin1','12345678912','admin1@admin.ca','Cape Breton, Nova Scotia')
 # admin1.addEmployee('Emp1','12345678913','emp1@emp.ca','Cape Breton, Nova Scotia','35','Sr. Engineer','150K')
 # admin1.viewEmployee()
-res1=Restaurent('Woodroad')
+menu1=Menu()
+menu1.addItem('Cappuccino','5.95','20')
+menu1.addItem('Latte','4.97','20')
+menu1.addItem('Americano','5.95','20')
+res1=Restaurent('\tWoodroad')
+Customer1=Customer('Customer1','012345678912','customer1@customer.com','Cape Breton')
+admin1.addNewItem(res1,Item('Cappuccino','5.95','20'))
+admin1.addNewItem(res1,Item('Latte','4.97','20'))
+admin1.addNewItem(res1,Item('Americano','5.95','20'))
+Customer1.UserShowItem(res1)
+# Customer1.UserViewCart()
